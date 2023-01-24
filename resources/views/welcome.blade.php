75849c13adf622407e08686c0dde03b342a3d153
Ali Ather <67648938+aliatherpeerzada@users.noreply.github.com>
2023-01-24 17:04:09 +0500

    new theme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,96 +1,44 @@
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ URL::asset('assets/img/apple-icon.png') }}">
-    <link rel="icon" type="image/png" href="{{ URL::asset('assets/img/favicon.png') }}">
-    <title>
-        Automaxed
-    </title>
-    <script src="//unpkg.com/alpinejs" defer></script>
-
-    <!--     Fonts and icons     -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-    <!-- Nucleo Icons -->
-    <link href="{{ URL::asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
-    <link href="{{ URL::asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
-    <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <link href="{{ URL::asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
-    <!-- CSS Files -->
-    <link id="pagestyle" href="{{ URL::asset('assets/css/argon-dashboard.css') }}" rel="stylesheet" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>License Manager Login</title>
+    <link rel="stylesheet" href="{{URL::asset('assets/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('assets/css/styles.min.css')}}">
 </head>
 
-<body class="">
-    <main class="main-content position-relative border-radius-lg ">
-        <div class="container-fluid py-4">
-  <div class="row">
-    <div class="col-md-3"></div>
-            <div class="col-md-6 mt-4 d-flex flex-column mx-lg-0 mx-auto">
-                <div class="card card-plain">
-                  <div class="card-header pb-0 text-start">
-                    <h4 class="font-weight-bolder text-center">{{strip_tags(App\Models\User::pluck('companyname')->first())}}</h4>
-                    <p class="mb-0 text-center">Enter your Credentials to sign in</p>
-                  </div>
-                  <div class="card-body">
-                    <form role="form" action="{{route('login')}}" method="POST">
-                      <div class="mb-3">
-                        <input type="text" class="form-control form-control-lg @error('username') is-invalid @enderror" name="username" placeholder="User Name..." aria-label="username">
-                        @error('username')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror      
+<body class="bg-gradient-primary">
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-sm-12 col-md-9 col-lg-7 col-xl-6 col-xxl-5">
+                <div class="card shadow-lg o-hidden border-0 my-5">
+                    <div class="card-body p-0">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h3 class="text-dark">{!!App\Models\User::pluck('companyname')->first()!!}</h3>
+                               
+                                <hr>
+                                <p class="mb-3 text-primary fw-bold">Enter your credentials to sign in</p>
+                                <hr class="mb-4">
+                            </div>
+                            <form class="user" action="{{route('login')}}" method="POST">
+                              @csrf
+                                <div class="mb-3"><input class="form-control form-control-user" type="text" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Admin Username" name="username"></div>
+                                <div class="mb-3"><input class="form-control form-control-user" type="password" id="exampleInputPassword" placeholder="Admin Password" name="password"></div>
+                                <div class="mb-3"><input class="form-control form-control-user" type="password" id="exampleInputPassword-1" placeholder="Secret Phrase" name="secret"></div>
+                                <div class="mt-4"><button class="btn btn-primary d-block btn-user w-100" type="submit">Login</button></div>
+                            </form>
+                        </div>
                     </div>
-@csrf
-                      <div class="mb-3">
-                        <input type="password" name="password" class="form-control form-control-lg @error('password') is-invalid @enderror"  placeholder="Password" aria-label="Password">
-                        @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror    
-                    </div>
-                      <div class="mb-3">
-                        <input type="text" class="form-control form-control-lg @error('secret') is-invalid @enderror"  name="secret" placeholder="Secret Phrase..." aria-label="secret">
-                        @error('secret')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror  
-                    </div>
-                      <div class="text-center">
-                        <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Sign in</button>
-                      </div>
-                      <x-message />
-                    </form>
-                  </div>
-                 </div>
-              </div>
+                </div>
             </div>
-            <div class="col-md-3"></div>
-
         </div>
- 
-    </main>
-    <!--   Core JS Files   -->
-    <script src="{{ URL::asset('assets/js/core/popper.min.js') }}"></script>
-    <script src="{{ URL::asset('assets/js/core/bootstrap.min.js') }}"></script>
-    <script src="{{ URL::asset('assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
-    <script src="{{ URL::asset('assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
-    <script src="{{ URL::asset('assets/js/plugins/chartjs.min.js') }}"></script>
-    <script>
-        var win = navigator.platform.indexOf('Win') > -1;
-        if (win && document.querySelector('#sidenav-scrollbar')) {
-            var options = {
-                damping: '0.5'
-            }
-            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-        }
-    </script>
-    <script src="{{ URL::asset('assets/js/argon-dashboard.min.js') }}"></script>
+    </div>
+    <script src="{{URL::asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{URL::asset('assets/js/script.min.js')}}"></script>
 </body>
 
 </html>
